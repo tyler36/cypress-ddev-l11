496ffeef83303c5ca5445aaf1f5feb8286864143
tyler36 <7234392+tyler36@users.noreply.github.com>
2024-06-18 16:34:01 +0900

    add viewport

--- cypress.config.js
+++ cypress.config.js
@@ -3,6 +3,12 @@ export default {
   videosFolder: 'logs/video',
   screenshotOnRunFailure: true,
   screenshotsFolder: 'logs/screenshots',
+  // Mobile viewport
+  viewportWidth: 375,
+  viewportHeight: 667,
+  // Desktop
+  // viewportWidth: 1920,
+  // viewportHeight: 1080,
   e2e: {
     setupNodeEvents(on, config) {
       // implement node event listeners here
