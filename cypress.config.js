144a673f8fe1af9e5bb5c7eef4e683224a1d9044
tyler36 <7234392+tyler36@users.noreply.github.com>
2024-06-18 16:25:07 +0900

    add cypress media

--- cypress.config.js
+++ cypress.config.js
@@ -1,4 +1,8 @@
 export default {
+  video: true,
+  videosFolder: 'logs/video',
+  screenshotOnRunFailure: true,
+  screenshotsFolder: 'logs/screenshots',
   e2e: {
     setupNodeEvents(on, config) {
       // implement node event listeners here
--- cypress/e2e/Welcome.cy.js
+++ cypress/e2e/Welcome.cy.js
@@ -1,6 +1,7 @@
 describe('template spec', () => {
   it('passes', () => {
     cy.visit('/')
+    cy.screenshot()
     cy.get('footer')
       .should('contain', 'Laravel v11.5.0')
       .should('contain', 'PHP v8.2.20')
