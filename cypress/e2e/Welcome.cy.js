d007abd6e0703a0bb86da4dd1ac9d2709be20f07
tyler36 <7234392+tyler36@users.noreply.github.com>
2024-06-18 15:57:40 +0900

    Welcome test

---
+++ cypress/e2e/Welcome.cy.js
@@ -0,0 +1,8 @@
+describe('template spec', () => {
+  it('passes', () => {
+    cy.visit('/')
+    cy.get('footer')
+      .should('contain', 'Laravel v11.5.0')
+      .should('contain', 'PHP v8.2.20')
+  })
+})
