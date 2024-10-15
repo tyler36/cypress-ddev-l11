b0ad7d469c0720bda69eb4bcbf4b1c25305e4ef8
tyler36 <7234392+tyler36@users.noreply.github.com>
2024-10-15 15:24:47 +0900

    extend example tests

--- tests/e2e/Nihongo.cy.js
+++
@@ -1,8 +0,0 @@
-describe('Japanese locale', () => {
-  it('日本語テスト', () => {
-    cy.visit('https://iqnet.co.jp')
-    cy.screenshot()
-    cy.get('section.footer')
-      .should('contain', '株式会社インタークエスト')
-  })
-})
